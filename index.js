49dcd1ec1675262dff8ce8af714dde72f5e35faa
root <root@_HOSTNAME_>
2022-03-06 12:00:26 +0100

    express

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const client = new Client({
   ],
 });
 
+require("express")()
+  .get("/", (_, res) => res.send("Works"))
+  .listen(80);
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
